2999466254801f31f5c5cfb0b8bda044bcc6e854
GTFAN311 <bauerc2371@student.faytechcc.edu>
2026-09-15 20:32:52 -0400

    First Python assignment
    Taking input from user and printing output.

---
+++ P1LAB1_BauerCurtis.py
@@ -0,0 +1,9 @@
+#Curtis Bauer
+#15 Sept 2026
+#P1LAB1
+#Input/output using Python programming language in Visual Studio Code.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name +"!", "Welcome to CTI-110!")
